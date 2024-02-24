ba4762e8aa2005dada68ca5680c28472757f2e72
joykepler <joyjoykepler@gmail.com>
2024-02-24 20:56:21 -0300

    php

--- sistema.php
+++ sistema.php
@@ -46,6 +46,7 @@
         .bemvindo {
             margin: 4rem;
         }
+
     </style>
 </head> 
 <body>
@@ -58,7 +59,14 @@
         echo "<h1>Bem vindo <u>$logado</u></h1>";
     ?>
 </div>
-
+<div class="flex justify-center mb-16 gap-3">
+    <input type="search" placeholder="Pesquisar" id="pesquisar" class="rounded w-64">
+    <button>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
+        </svg>
+    </button>
+</div>
 <div class="overflow-x-auto">
   <table class="table-auto border-collapse w-full">
     <thead>
